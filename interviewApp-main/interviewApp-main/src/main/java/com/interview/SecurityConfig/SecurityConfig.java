7338d8882a930b25ab742f293429bac73683788c
bachakMedAmin <bachakmohamed1@gmail.com>
2024-09-11 22:35:13 +0100

    csrf disabled and fixed

--- interviewApp-main/interviewApp-main/src/main/java/com/interview/SecurityConfig/SecurityConfig.java
+++ interviewApp-main/interviewApp-main/src/main/java/com/interview/SecurityConfig/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -19,7 +20,7 @@ public class SecurityConfig  {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
+        http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/**").permitAll()
 
